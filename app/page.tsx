d9f1fc64646fcc0238a4de24d06037e27efa8ae9
Cristian Gómez <cgomezcardenas15@gmail.com>
2026-07-13 12:41:25 -0500

    Primer proyecto funcionando

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🚀 Bienvenido a la Tienda Web de Cristian</h1>
+      <p>Este es el primer paso de nuestro proyecto.</p>
+    </main>
+  );
+}
